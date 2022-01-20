529282a2e47260c40f869b9dccfab2ff08342e37
Jorge Chaves <jorge.chaves@me.com>
2022-01-20 09:30:42 -0600

    .

--- dist/app.js
+++ dist/app.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
+const body_parser_1 = __importDefault(require("body-parser"));
 const app = (0, express_1.default)();
 const http_1 = __importDefault(require("http"));
 const server = http_1.default.createServer(app);
@@ -17,18 +18,14 @@ const io = new socket_io_1.Server(server, {
 });
 const port = 3000;
 app.use((0, cors_1.default)());
+app.use(body_parser_1.default.urlencoded({ extended: false }));
+app.use(body_parser_1.default.json());
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
-app.get('/test', (req, res) => {
-    io.emit('probando api emisor', { valor: '111' });
+app.post('/test', (req, res) => {
+    io.emit('probando api emisor', req.body);
     res.sendStatus(200);
-});
-io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-        console.log('message: ' + msg);
-        socket.broadcast.emit('hola' + msg);
-    });
 });
 server.listen(port, () => {
     console.log('listening on *:3000');
