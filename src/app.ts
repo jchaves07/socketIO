cc303d88d929acf37e06dfa28d20c0161cccfd68
Jorge Chaves <jorge.chaves@me.com>
2022-01-20 00:33:41 -0600

    api prueba

--- src/app.ts
+++ src/app.ts
@@ -9,13 +9,18 @@ const port = 3000;
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
   });
-  
+  app.get('/test', (req, res) => {
+    io.emit('probando api emisor', { valor: '111' });
+    res.sendStatus(200);
+  });
   
   io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
       console.log('message: ' + msg);
+      socket.broadcast.emit('hola' + msg);
     });
   });
   server.listen(port, () => {
     console.log('listening on *:3000');
   });
+
