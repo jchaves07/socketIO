b256a8e8e16ada69b007d456f496767a8a2f54e4
Jorge Chaves <jorge.chaves@me.com>
2022-01-20 01:12:03 -0600

    cors

--- dist/app.js
+++ dist/app.js
@@ -9,19 +9,18 @@ const app = (0, express_1.default)();
 const http_1 = __importDefault(require("http"));
 const server = http_1.default.createServer(app);
 const socket_io_1 = require("socket.io");
-const io = new socket_io_1.Server(server);
+const io = new socket_io_1.Server(server, {
+    allowRequest: (req, callback) => {
+        const noOriginHeader = req.headers.origin === undefined;
+        callback(null, noOriginHeader);
+    }
+});
 const port = 3000;
 app.use((0, cors_1.default)());
 app.get('/', (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
     res.sendFile(__dirname + '/public/index.html');
 });
 app.get('/test', (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
     io.emit('probando api emisor', { valor: '111' });
     res.sendStatus(200);
 });
--- src/app.ts
+++ src/app.ts
@@ -4,21 +4,21 @@ const app = express();
 import http from 'http';
 const server = http.createServer(app);
 import { Server } from "socket.io";
-const io = new Server(server);
+const io = new Server(server, {
+    allowRequest: (req, callback) => {
+      const noOriginHeader = req.headers.origin === undefined;
+      callback(null, noOriginHeader);
+    }
+  });
 const port = 3000;
 
 app.use(cors());
 app.get('/', (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+
 
     res.sendFile(__dirname + '/public/index.html');
   });
   app.get('/test', (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
     io.emit('probando api emisor', { valor: '111' });
     res.sendStatus(200);
   });
